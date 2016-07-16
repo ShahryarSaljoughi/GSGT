7f6648f5f98ea31493637a9ce96c816d7832514d
shahryar <s.shahryar75@gmail.com>
2016-07-17 02:11:03 +0430

    extra "print"s got omitted !

--- NodeClass.py
+++ NodeClass.py
@@ -14,15 +14,15 @@ def Str2Tree(preOrderedString,inOrderedString):
     preStr=str(preOrderedString)
     inStr = str(inOrderedString)
     root= preStr[0]
-    print "root is ",root """ PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
+    print "root is ",root
     node = Node(root)
     if (preStr==inStr and len(preStr)==len(inStr)==1) :
         return node
 
     inOrderStrOfLeftTree=inStr.partition(root)[0]
-    print "inorder string of left tree is ",inOrderStrOfLeftTree""" PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
+    print "inorder string of left tree is ",inOrderStrOfLeftTree
     inOrderStrOfRightTree = inStr.partition(root)[2]
-    print "inorder string of right tree is ", inOrderStrOfRightTree""" PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
+    print "inorder string of right tree is ", inOrderStrOfRightTree
     lenofleft=len(inOrderStrOfLeftTree)
     lenOfRight = len(inOrderStrOfRightTree)
     preOrderStrOfLeftTree=preStr[1:lenofleft+1]

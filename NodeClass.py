62347ab69dae7aa487f2a4ad57cfabc521c9682f
shahryar <s.shahryar75@gmail.com>
2016-07-17 02:10:11 +0430

    bug fixed : Programme just works fine .  PRINTS admit it !!

--- NodeClass.py
+++ NodeClass.py
@@ -9,22 +9,26 @@ class Node():
 def Str2Tree(preOrderedString,inOrderedString):
     """
 
-    :rtype : object
+    :rtype : object of Node class
     """
     preStr=str(preOrderedString)
     inStr = str(inOrderedString)
     root= preStr[0]
+    print "root is ",root """ PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
     node = Node(root)
     if (preStr==inStr and len(preStr)==len(inStr)==1) :
         return node
 
     inOrderStrOfLeftTree=inStr.partition(root)[0]
+    print "inorder string of left tree is ",inOrderStrOfLeftTree""" PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
     inOrderStrOfRightTree = inStr.partition(root)[2]
-    lenOfLeft=len(inOrderStrOfLeftTree)
+    print "inorder string of right tree is ", inOrderStrOfRightTree""" PPPPPPPPPRRRRRRRRRRRRRIIIIIIIIIIIIINNNNNNNNNNNNTTTTTTTTTTTTT"""
+    lenofleft=len(inOrderStrOfLeftTree)
     lenOfRight = len(inOrderStrOfRightTree)
-    preOrderStrOfLeftTree=preStr[1:lenOfLeft]
-    preOrderStrOfRightTree=preStr[lenOfLeft+1:]
-    if lenOfLeft>0:
+    preOrderStrOfLeftTree=preStr[1:lenofleft+1]
+
+    preOrderStrOfRightTree=preStr[lenofleft+1:]
+    if lenofleft>0:
         node.left=Str2Tree(preOrderStrOfLeftTree,inOrderStrOfLeftTree)
     if lenOfRight>0:
         node.right=Str2Tree(preOrderStrOfRightTree,inOrderStrOfRightTree)
